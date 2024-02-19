cb9a2fef829cdb8d34ac96bc2e73366d59da32f0
Eeemlo <emma.lorensson@outlook.com>
2024-02-19 15:09:02 +0100

    Korrekt data i diagram

--- src/js/main.js
+++ src/js/main.js
@@ -1,101 +1,85 @@
-const navbar = document.getElementById('my-nav');
+const navbar = document.getElementById("my-nav");
 window.onscroll = () => {
-    if (window.scrollY > 100) {
-        navbar.classList.add('nav-active');
-    } else {
-        navbar.classList.remove('nav-active');
-    }
+  if (window.scrollY > 100) {
+    navbar.classList.add("nav-active");
+  } else {
+    navbar.classList.remove("nav-active");
+  }
 };
 
 window.onload = function () {
+  const hamburgerEl = document.querySelector(".hamburger");
+  const mobileNavEl = document.querySelector(".mobile_nav");
 
-    const hamburgerEl = document.querySelector('.hamburger')
-    const mobileNavEl = document.querySelector('.mobile_nav');
-
-    hamburgerEl.addEventListener('click', function () {
-        hamburgerEl.classList.toggle('active');
-        mobileNavEl.classList.toggle('active');
-    })
-}
-
+  hamburgerEl.addEventListener("click", function () {
+    hamburgerEl.classList.toggle("active");
+    mobileNavEl.classList.toggle("active");
+  });
+};
 
 /*DIAGRAM*/
 
-import Chart from 'chart.js/auto';
-
-const ctx1 = document.getElementById('myChart1');
-
-new Chart(ctx1, {
-  type: 'bar',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
-    datasets: [{
-      label: 'Antal sökande',
-      data: [12, 19, 3, 5, 2, 3],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
-
-const ctx2 = document.getElementById('myChart2');
-
-new Chart(ctx2, {
-  type: 'pie',
-  data: {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple'],
-    datasets: [{
-      label: '# of Votes',
-      data: [12, 19, 3, 5, 2],
-      borderWidth: 1
-    }]
-  },
-  options: {
-    scales: {
-      y: {
-        beginAtZero: true
-      }
-    }
-  }
-});
+import Chart from "chart.js/auto";
 
 //Variabler för att lagra data
 const url = `https://studenter.miun.se/~mallar/dt211g/`;
 let data = [];
 
-
 async function fetchCourses() {
   //Gör fetchanrop
   const response = await fetch(url);
   //Lagra svar i variabeln data
-  data = await response.json();  
-  
-  //Filtrera ut kurser från datan
-const courses = data.filter(function(types) {
-  return types.type === "Kurs"
-});
+  data = await response.json();
 
-console.log("kurser filtrerade..")
+  //Filtrera ut kurser från datan
+  const courses = data.filter(function (types) {
+    return types.type === "Kurs";
+  });
 
   //Sortera data efter antal sökande
-courses.sort(function(a, b) {
-  return b.applicantsTotal - a.applicantsTotal;
-});
+  courses.sort(function (a, b) {
+    return b.applicantsTotal - a.applicantsTotal;
+  });
 
-console.log("data sorterad..");
+  //Stapeldiagram
 
-for (let i = 0; i <= 5; i++) {
+  const ctx1 = document.getElementById("myChart1");
 
-console.log(courses[i]) 
+  new Chart(ctx1, {
+    type: "bar",
+    data: {
+      labels: [
+        courses[0].name,
+        courses[1].name,
+        courses[2].name,
+        courses[3].name,
+        courses[4].name,
+        courses[5].name,
+      ],
+      datasets: [
+        {
+          label: "Antal sökande",
+          data: [
+            courses[0].applicantsTotal,
+            courses[1].applicantsTotal,
+            courses[2].applicantsTotal,
+            courses[3].applicantsTotal,
+            courses[4].applicantsTotal,
+            courses[5].applicantsTotal,
+          ],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
 }
-
-};
 
 fetchCourses();
 
@@ -103,23 +87,52 @@ async function fetchProgrammes() {
   //Gör fetchanrop
   const response = await fetch(url);
   //Lagra svar i variabeln data
-  data = await response.json();  
-  
-  //Filtrera ut kurser från datan
-const programmes = data.filter(function(types) {
-  return types.type === "Program"
-});
+  data = await response.json();
 
-console.log("Program filtrerade..")
+  //Filtrera ut kurser från datan
+  const programmes = data.filter(function (types) {
+    return types.type === "Program";
+  });
 
   //Sortera data efter antal sökande
-programmes.sort(function(a, b) {
-  return b.applicantsTotal - a.applicantsTotal;
-});
+  programmes.sort(function (a, b) {
+    return b.applicantsTotal - a.applicantsTotal;
+  });
 
-console.log("data sorterad..");
+  //Cirkeldiagram
 
+  const ctx2 = document.getElementById("myChart2");
 
+  new Chart(ctx2, {
+    type: "pie",
+    data: {
+      labels: [
+        programmes[0].name,
+        programmes[1].name,
+        programmes[2].name,
+        programmes[3].name,
+        programmes[4].name,
+      ],
+      datasets: [
+        {
+          label: "Antal sökande",
+          data: [        programmes[0].applicantsTotal,
+          programmes[1].applicantsTotal,
+          programmes[2].applicantsTotal,
+          programmes[3].applicantsTotal,
+          programmes[4].applicantsTotal],
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  });
 }
 
 fetchProgrammes();
